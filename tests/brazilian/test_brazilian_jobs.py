30785ea25d7bea8dd22ac98bd3b88688c46b9a3d
Geovana Augusta <geovanaag@hotmail.com>
2022-12-06 22:33:10 -0300

    test: read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file("tests/mocks/brazilians_jobs.csv")[2] == {
+        "title": "Analista de Software",
+        "salary": "4000",
+        "type": "full time",
+    }
+
+
+# SOURCE 11
+# Exercício 4 - Testes
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,3 +3,6 @@ from src.pre_built.counter import count_ocurrences
 
 def test_counter():
     assert count_ocurrences("data/jobs.csv", "Python") == 1639
+
+# SOURCE 10
+# Exercício 4 - Testes
