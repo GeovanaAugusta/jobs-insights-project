a8f6373cd05985408521f48f901029eb06fc2794
Geovana Augusta <geovanaag@hotmail.com>
2022-12-06 21:11:22 -0300

    feat: def that filters a list of jobs by job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -28,21 +28,14 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    filtered_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    for job in jobs:
+        # Coluna job_type corresponde ao param job_type
+        if job["job_type"] == job_type:
+            filtered_jobs.append(job)
+            return filtered_jobs
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
 
 # SOURCE 1
 # https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/290e715d-73e3-4b2d-a3c7-4fe113474070/section/b436f9e0-dfde-4a16-9bad-82f0c559dd45/day/61e88b4a-b97a-4f96-b5a0-abaa50651e37/lesson/293603be-ede4-41d6-8921-963ecdb0bc44
@@ -65,3 +58,17 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
 # https://www.programiz.com/python-programming/methods/set/add
 # https://www.w3schools.com/python/ref_func_set.asp#:~:text=The%20set()%20function%20creates,in%20the%20chapter%20Python%20Sets.
 # https://www.programiz.com/python-programming/methods/list/append
+
+# SOURCE 6
+# Como filtrar uma lista
+# https://www.pythontutorial.net/python-basics/python-filter-list/
+
+# scores = [70, 60, 80, 90, 50]
+
+# filtered = []
+
+# for score in scores:
+#     if score >= 70:
+#         filtered.append(score)
+
+# print(filtered)
